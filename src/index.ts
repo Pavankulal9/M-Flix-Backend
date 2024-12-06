c6c7fbef9fe4210761c4fdd3525d73f1f9af06d8
Pavan <pawankulal9@gmail.com>
2024-12-06 20:12:03 +0530

    creating and https server for ec2 instance

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import https from "https";
+import fs from 'fs';
 import {Server} from "net";
 import createApplication from "@/app";
 import { AppConfig } from "@/config";
@@ -6,12 +8,15 @@ import { Logger } from "@/services";
 function startServer():Server{
   const app = createApplication();
 
-  return app.listen(AppConfig.App.PORT,async()=>{
+  const options = {
+  key: fs.readFileSync("/etc/ssl/private/selfsigned.key"),
+  cert: fs.readFileSync("/etc/ssl/certs/selfsigned.crt"),
+ };
 
+  return https.createServer(options, app).listen(process.env.PORT, () => {
     Logger.debug(` App ${AppConfig.App.NAME} with api version ${AppConfig.App.API_VERSION} is starting
-    `);
+            `);
     Logger.debug(`App is listening on PORT:${AppConfig.App.PORT}`);
-    
   });
 }
 
