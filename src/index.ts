6667fe39d05df8cebb57ebee0110c5e1bb11d662
Pavan <pawankulal9@gmail.com>
2024-12-07 19:40:37 +0530

    changed https request to normal app listner request

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
-import https from "https";
-import fs from 'fs';
-import {Server} from "net";
+import { Server } from "net";
 import createApplication from "@/app";
 import { AppConfig } from "@/config";
 import { Logger } from "@/services";
 
-function startServer():Server{
+function startServer(): Server {
   const app = createApplication();
-
-  const options = {
-  key: fs.readFileSync("/etc/ssl/private/selfsigned.key"),
-  cert: fs.readFileSync("/etc/ssl/certs/selfsigned.crt"),
- };
-
-  return https.createServer(options, app).listen(process.env.PORT, () => {
+  return app.listen(AppConfig.App.PORT, async () => {
     Logger.debug(` App ${AppConfig.App.NAME} with api version ${AppConfig.App.API_VERSION} is starting
-            `);
+    `);
     Logger.debug(`App is listening on PORT:${AppConfig.App.PORT}`);
   });
 }
